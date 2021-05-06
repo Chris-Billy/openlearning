c6c75ea065376c148212139536d4aa0c3a97e07c
Mael <legrand.mael2002@gmail.com>
2021-05-06 15:07:50 +0200

    Début card Component (réutilisable)

---
+++ components/Card_courses.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="reco_card"><p>sdqsdsq</p></div>
+</template>
+<script>
+export default {
+	name: 'RecommandedCards'
+}
+</script>
+<style scoped>
+.reco_card {
+	width: 90%;
+	height: 120px;
+	border-radius: 20px;
+	background: white;
+}
+</style>
