c5f15ead021d5f8cf1833dafd2d294412dc568a2
Walid <walidhaddoury@outlook.fr>
2021-05-11 11:38:56 +0200

    resize des logos de la navbar en legerement plus petit

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -4,8 +4,8 @@
 	>
 		<NuxtLink to="home">
 			<svg
-				width="27"
-				height="27"
+				width="22"
+				height="22"
 				viewBox="0 0 22 22"
 				fill="none"
 				xmlns="http://www.w3.org/2000/svg"
@@ -26,8 +26,8 @@
 		</NuxtLink>
 		<NuxtLink to="">
 			<svg
-				width="27"
-				height="27"
+				width="22"
+				height="22"
 				viewBox="0 0 23 22"
 				fill="none"
 				xmlns="http://www.w3.org/2000/svg"
@@ -48,8 +48,8 @@
 		</NuxtLink>
 		<NuxtLink to="">
 			<svg
-				width="27"
-				height="27"
+				width="22"
+				height="22"
 				viewBox="0 0 23 22"
 				fill="none"
 				xmlns="http://www.w3.org/2000/svg"
@@ -70,8 +70,8 @@
 		</NuxtLink>
 		<NuxtLink to="">
 			<svg
-				width="27"
-				height="27"
+				width="22"
+				height="22"
 				viewBox="0 0 22 22"
 				fill="none"
 				xmlns="http://www.w3.org/2000/svg"
