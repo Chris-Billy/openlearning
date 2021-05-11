e201f8cc5853afe1be216a8a38f4048ead1494d4
Mael <legrand.mael2002@gmail.com>
2021-05-12 00:30:23 +0200

    Passage en tailwind de la page d'acceuil et ses composants

--- components/CardCourses.vue
+++ components/CardCourses.vue
@@ -1,31 +1,45 @@
 <template>
-	<div class="reco_card">
-		<div class="card">
-			<div class="left_card">
-				<div class="card_title">
-					<p>{{ cardTitle }}</p>
+	<div class="flex flex-row justify-center">
+		<div class="w-11/12 h-32 mb-5 flex rounded-lg bg-white flex-row">
+			<div class="h-full w-9/12 flex justify-center items-center flex-col">
+				<div class="text-sm w-11/12 h-1/5">
+					<p class="whitespace-nowrap overflow-x-auto">{{ cardTitle }}</p>
 				</div>
-				<div class="card_infos">
-					<div class="card_stars">
-						<img src="~/static/star.png" />
+				<div class="flex flex-row w-full">
+					<div class="flex justify-evenly flex-row w-1/4 items-center">
+						<img class="h-3 w-3" src="~/static/star.png" />
 						<p>{{ nbStar }}</p>
 					</div>
-					<div class="card_owner">
-						<div class="separator"></div>
-						<div class="owner_text">
+					<div class="flex flex-row justify-center items-center">
+						<div class="h-1 w-1 rounded-full bg-gray-400 mr-1"></div>
+						<div
+							class="
+								text-xs text-gray-400
+								flex flex-row
+								whitespace-nowrap
+								overflow-x-auto
+							"
+						>
 							<p>Par {{ owner }}</p>
 						</div>
-						<div class="separator"></div>
+						<div class="h-1 w-1 rounded-full bg-gray-400 ml-1 mr-1"></div>
 					</div>
-					<div clas="card_cat">
-						<div class="catego">
-							<p>{{ category }}</p>
+					<div clas="flex justify-center items-center">
+						<div
+							class="
+								text-xs text-gray-400
+								flex flex-row
+								justify-center
+								items-center
+							"
+						>
+							<p class="mt-1">{{ category }}</p>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="right_card">
-				<img src="~/static/template_image2.png" />
+			<div class="h-full w-1/6 flex justify-center items-center">
+				<img class="h-15 w-15 rounded-lg" src="~/static/template_image2.png" />
 			</div>
 		</div>
 	</div>
@@ -36,109 +50,4 @@ export default {
 	props: ['cardTitle', 'nbStar', 'owner', 'category']
 }
 </script>
-<style scoped>
-.reco_card {
-	display: flex;
-	justify-content: center;
-	flex-direction: row;
-}
-.card {
-	width: 90%;
-	height: 120px;
-	margin-bottom: 5%;
-	display: flex;
-	border-radius: 20px;
-	background: white;
-	flex-direction: row;
-}
-.left_card {
-	height: 100%;
-	width: 70%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;
-}
-.card_title {
-	font-family: 'DM Sans', sans-serif;
-	font-size: 14px;
-	width: 90%;
-	height: 20%;
-	overflow: hidden;
-}
-.card_title p {
-	font-family: 'DM Sans', sans-serif;
-	font-size: 14px;
-	white-space: nowrap;
-	/* animation: scrollingText 5s linear infinite;
-	animation-direction: alternate; */
-}
-.card_stars {
-	display: flex;
-	justify-content: space-evenly;
-	flex-direction: row;
-	width: 25%;
-}
-.card_stars img {
-	height: 13px;
-	width: 13px;
-}
-.card_stars p {
-	font-size: 12px;
-}
-.separator {
-	height: 5px;
-	width: 5px;
-	border-radius: 9000px;
-	background: #91919f;
-	margin-left: 5px;
-	margin-right: 5px;
-}
-.card_owner {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-direction: row;
-	margin-left: -5px;
-}
-.owner_text {
-	font-size: 12px;
-	color: #91919f;
-	display: flex;
-	flex-direction: row;
-}
-.owner_text p {
-	overflow: hidden;
-	width: 100%;
-}
-.card_infos {
-	display: flex;
-	flex-direction: row;
-	width: 100%;
-}
-.catego p {
-	font-size: 12px;
-	color: #91919f;
-}
-.right_card {
-	height: 100%;
-	width: 30%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-}
-.right_card img {
-	height: 75px;
-	width: 75px;
-	border-radius: 15px;
-}
-
-/* @keyframes scrollingText {
-	0% {
-		transform: translate(2%, 0);
-	}
-	100% {
-		transform: translate(-30%, 0);
-	}
-} */
-</style>
+<style scoped></style>
