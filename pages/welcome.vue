e62e08563d784f89f19066626445e35083204141
Mael <legrand.mael2002@gmail.com>
2021-06-07 16:21:31 +0200

    Push page Welcome (responsive)

--- pages/welcome.vue
+++ pages/welcome.vue
@@ -1,62 +1,87 @@
 <template>
-	<div
-		class="
-			example
-			h-full
-			w-screen
-			overflow-y-scroll overflow-x-hidden
-			flex flex-col
-		"
-	>
-		<div class="img-bg h-1/4 w-full">
-			<div class="top_bar w-full h-1/5 flex flex-row justify-around">
-				<div class="logo_part h-full w-1/3 flex items-center ml-3">
+	<div class="content h-full w-full">
+		<header class="body-font">
+			<div
+				class="
+					container
+					mx-auto
+					flex flex-wrap
+					p-5
+					flex-col
+					md:flex-row
+					items-center
+				"
+			>
+				<a
+					class="
+						flex
+						title-font
+						font-medium
+						items-center
+						text-white
+						mb-4
+						md:mb-0
+					"
+				>
 					<svg
-						width="72"
-						height="83"
-						viewBox="0 0 72 83"
+						width="95"
+						height="107"
+						viewBox="0 0 95 107"
 						fill="none"
+						class="w-16 h-16 text-white p-2"
 						xmlns="http://www.w3.org/2000/svg"
 					>
 						<path
-							d="M54.584 37.8688C57.8724 41.5546 60.0078 46.1129 60.7289 50.9856C61.4499 55.8583 60.7253 60.8342 58.6438 65.3042C56.5622 69.7742 53.214 73.5444 49.0088 76.1535C44.8037 78.7626 39.924 80.0974 34.9671 79.9945C30.0102 79.8915 25.1909 78.3554 21.0992 75.574C17.0075 72.7927 13.8208 68.8867 11.9291 64.3342C10.0374 59.7817 9.52272 54.7801 10.4481 49.9416C11.3735 45.1031 13.6988 40.6375 17.1394 37.0913C17.4853 36.7377 54.2507 37.5001 54.584 37.8688Z"
+							d="M70.73 48.5692C74.8405 53.1766 77.5098 58.8744 78.4111 64.9653C79.3124 71.0562 78.4066 77.276 75.8047 82.8635C73.2027 88.451 69.0174 93.1638 63.761 96.4252C58.5046 99.6866 52.4051 101.355 46.2089 101.226C40.0127 101.098 33.9887 99.1775 28.874 95.7008C23.7594 92.2241 19.776 87.3417 17.4114 81.651C15.0467 75.9604 14.4034 69.7085 15.5601 63.6603C16.7169 57.6121 19.6235 52.0301 23.9243 47.5974C24.3566 47.1553 70.3134 48.1084 70.73 48.5692Z"
 							stroke="white"
 							stroke-width="5"
 						/>
 						<path
-							d="M15.0002 36.9417V24.7715C15.0005 24.6479 15.0273 24.5262 15.0784 24.4166C15.1294 24.3069 15.2032 24.2126 15.2936 24.1413C15.384 24.0701 15.4883 24.0242 15.5979 24.0073C15.7074 23.9904 15.819 24.0031 15.9233 24.0442L34.4078 31.727C34.4814 31.7588 34.5596 31.7754 34.6386 31.776H38.0309C38.1094 31.7757 38.1874 31.7609 38.2616 31.7322L56.0769 24.6735C56.1812 24.6323 56.2928 24.6196 56.4023 24.6365C56.5119 24.6534 56.6162 24.6994 56.7066 24.7706C56.797 24.8418 56.8708 24.9362 56.9218 25.0458C56.9729 25.1555 56.9997 25.2772 57 25.4008V36.9443C56.9997 37.0842 56.9654 37.2215 56.9008 37.3414C56.8361 37.4613 56.7436 37.5593 56.6331 37.6251C40.2901 47.2859 22.5832 41.7876 15.3602 37.6251C15.2491 37.5601 15.1565 37.4617 15.0928 37.3409C15.0292 37.2201 14.9971 37.0818 15.0002 36.9417Z"
+							d="M21.2503 48.6771V33.4644C21.2506 33.3099 21.2841 33.1578 21.3479 33.0207C21.4118 32.8837 21.504 32.7657 21.617 32.6767C21.73 32.5877 21.8604 32.5302 21.9973 32.5091C22.1343 32.488 22.2738 32.5038 22.4041 32.5553L45.5098 42.1588C45.6018 42.1986 45.6995 42.2193 45.7982 42.22H50.0386C50.1368 42.2196 50.2343 42.2011 50.327 42.1652L72.5962 33.3419C72.7265 33.2904 72.866 33.2745 73.0029 33.2957C73.1399 33.3168 73.2703 33.3743 73.3833 33.4633C73.4963 33.5523 73.5885 33.6703 73.6523 33.8073C73.7161 33.9444 73.7496 34.0965 73.75 34.251V48.6803C73.7496 48.8553 73.7067 49.0268 73.626 49.1767C73.5452 49.3266 73.4296 49.4492 73.2914 49.5314C52.8626 61.6074 30.7291 54.7345 21.7003 49.5314C21.5613 49.4502 21.4456 49.3271 21.366 49.1761C21.2865 49.0251 21.2464 48.8523 21.2503 48.6771Z"
 							fill="white"
 						/>
 						<path
-							d="M0.914466 14.8244C0.643206 14.7234 0.409921 14.5421 0.246324 14.3052C0.0827266 14.0682 -0.00324578 13.7871 9.37285e-05 13.5001C0.00343324 13.213 0.0959228 12.9339 0.264989 12.7008C0.434055 12.4676 0.671496 12.2917 0.945034 12.197L36.0036 0.079929C36.3079 -0.026643 36.6398 -0.026643 36.9441 0.079929L71.0715 12.2109C71.3406 12.3079 71.5735 12.4837 71.7394 12.715C71.9052 12.9462 71.996 13.222 71.9999 13.5056C72.0037 13.7892 71.9203 14.0673 71.7608 14.3029C71.6013 14.5384 71.3732 14.7203 71.1068 14.8244L36.977 27.9081C36.653 28.0306 36.2947 28.0306 35.9707 27.9081L0.914466 14.8244Z"
+							d="M3.64308 21.0305C3.30401 20.9043 3.0124 20.6777 2.8079 20.3815C2.60341 20.0853 2.49594 19.7339 2.50012 19.3751C2.50429 19.0163 2.6199 18.6674 2.83124 18.376C3.04257 18.0846 3.33937 17.8647 3.68129 17.7462L47.5045 2.59991C47.8848 2.4667 48.2998 2.4667 48.6801 2.59991L91.3394 17.7637C91.6758 17.8849 91.9669 18.1046 92.1742 18.3937C92.3815 18.6828 92.495 19.0275 92.4998 19.382C92.5046 19.7366 92.4004 20.0841 92.201 20.3786C92.0016 20.673 91.7165 20.9004 91.3835 21.0305L48.7213 37.3851C48.3162 37.5383 47.8684 37.5383 47.4633 37.3851L3.64308 21.0305Z"
 							fill="white"
 						/>
 						<path
-							d="M67 34.3749C67 34.1537 67.0582 33.9359 67.1696 33.7398C67.281 33.5437 67.4424 33.3752 67.64 33.2486C67.8647 33.1043 68.1283 33.0194 68.402 33.0029C68.6757 32.9865 68.9491 33.0392 69.1924 33.1553C69.4357 33.2715 69.6397 33.4465 69.782 33.6616C69.9244 33.8766 69.9998 34.1233 70 34.3749V60.6242C70 60.9891 69.842 61.339 69.5607 61.5971C69.2794 61.8551 68.8978 62 68.5 62C68.1022 62 67.7206 61.8551 67.4393 61.5971C67.158 61.339 67 60.9891 67 60.6242V34.3749Z"
+							d="M86.25 42.3019C86.25 42.0256 86.3227 41.7532 86.462 41.5081C86.6013 41.263 86.803 41.0524 87.05 40.8941C87.3309 40.7138 87.6603 40.6076 88.0025 40.587C88.3446 40.5665 88.6863 40.6324 88.9905 40.7775C89.2946 40.9227 89.5496 41.1416 89.7275 41.4103C89.9055 41.6791 89.9998 41.9875 90 42.3019V75.1137C90 75.5698 89.8025 76.0072 89.4508 76.3297C89.0992 76.6522 88.6223 76.8334 88.125 76.8334C87.6277 76.8334 87.1508 76.6522 86.7992 76.3297C86.4475 76.0072 86.25 75.5698 86.25 75.1137V42.3019Z"
 							fill="#FFD600"
 						/>
-						<path d="M67 24.0353L70 22V54H67V24.0353Z" fill="#FFD600" />
 						<path
-							d="M68.5 49C70.433 49 72 47.433 72 45.5C72 43.567 70.433 42 68.5 42C66.567 42 65 43.567 65 45.5C65 47.433 66.567 49 68.5 49Z"
+							d="M86.25 29.3775L90 26.8333V66.8333H86.25V29.3775Z"
+							fill="#FFD600"
+						/>
+						<path
+							d="M88.125 63.75C90.5412 63.75 92.5 61.7912 92.5 59.375C92.5 56.9588 90.5412 55 88.125 55C85.7087 55 83.75 56.9588 83.75 59.375C83.75 61.7912 85.7087 63.75 88.125 63.75Z"
 							fill="#FFD600"
 						/>
 					</svg>
-					<p class="font-semibold text-white text-3xl">OpenLearning</p>
-				</div>
-				<div class="login_search h-full w-1/3 flex items-center justify-center">
-					<div
+
+					<span class="ml-3 text-xl">OpenLearning</span>
+				</a>
+				<nav
+					class="
+						md:ml-auto
+						flex flex-wrap
+						items-center
+						text-base
+						justify-center
+					"
+				>
+					<a
 						class="
-							search_btn
-							rounded-full
+							mr-5
 							bg-white
+							rounded-full
 							h-10
 							w-10
 							flex
 							justify-center
 							items-center
+							hover:text-white
 						"
-					>
-						<svg
+						><svg
 							width="22"
 							height="22"
 							viewBox="0 0 22 22"
@@ -69,80 +94,241 @@
 								stroke-width="2"
 							/>
 						</svg>
-					</div>
-					<div
-						class="
-							bg-white
-							h-10
-							w-60
-							rounded-full
-							ml-5
-							flex
-							justify-center
-							items-center
-						"
-					>
-						<p class="font-semibold">Connectez-vous !</p>
-					</div>
-				</div>
+					</a>
+				</nav>
+				<button
+					class="
+						inline-flex
+						items-center
+						bg-white
+						border-0
+						py-1
+						px-3
+						h-10
+						text-black
+						focus:outline-none
+						hover:bg-gray-100
+						rounded-full
+						text-base
+						mt-4
+						md:mt-0
+					"
+				>
+					Connectez-vous
+				</button>
 			</div>
-			<div class="punchline h-4/5 w-full flex">
-				<div class="left w-1/2 h-full flex flex-col justify-evenly ml-48">
-					<p class="font-semibold text-6xl text-white w-1/2">
-						L’accès au savoir n’aura jamais été aussi simple.
-					</p>
-					<p class="font-semibold text-xl text-white w-1/2">
+		</header>
+		<section class="img-bg text-white body-font">
+			<div
+				class="
+					container
+					mx-auto
+					flex
+					px-5
+					py-24
+					md:flex-row
+					flex-col
+					items-center
+				"
+			>
+				<div
+					class="
+						lg:flex-grow
+						md:w-1/2
+						lg:pl-24
+						md:pl-16
+						flex flex-col
+						md:items-start
+						md:text-left
+						items-center
+						text-center
+					"
+				>
+					<h1 class="title-font sm:text-5xl text-4xl mb-4 font-medium">
+						L'accès au savoir
+						<br class="hidden lg:inline-block" />n'aura jamais été
+						<br class="hidden lg:inline-block" />aussi simple
+					</h1>
+					<p class="mb-8 leading-relaxed w-1/3">
 						N’attendez plus pour enrichir votre savoir à l’aide de nos cours
 						publiés par les plus grands experts.
 					</p>
-					<div
-						class="
-							h-24
-							w-64
-							rounded-lg
-							bg-white
-							flex
-							items-center
-							justify-center
-						"
-					>
-						<p class="font-semibold text-xl text-center">
+					<div class="flex justify-center">
+						<button
+							class="
+								inline-flex
+								text-black
+								font-semibold
+								bg-white
+								border-0
+								py-2
+								px-6
+								focus:outline-none
+								hover:bg-gray-600
+								hover:text-white
+								rounded-lg
+								text-lg
+							"
+						>
 							Inscrivez-vous dès maintenant !
-						</p>
+						</button>
 					</div>
 				</div>
-				<div class="right w-1/2 h-full"></div>
 			</div>
-		</div>
-		<div class="h-1/6 w-full flex flex-col space-y-5 items-center">
-			<p class="font-semibold text-white text-4xl mt-5">
-				Vous cherchez un cours ?
-			</p>
-			<p class="text-white text-xl">
-				Vous souhaitez apprendre un métier ou une compétence et vous ne savez
-				pas si le cours est disponible ? Faites une recherche !
-			</p>
-			<div class="w-2/5 h-12 bg-white rounded-full">
+		</section>
+		<section class="text-gray-400 bg-gray-900 body-font">
+			<div
+				class="
+					container
+					mx-auto
+					flex
+					px-5
+					py-24
+					md:flex-row
+					flex-col
+					items-center
+				"
+			>
 				<div
-					class="input_search w-full h-full flex justify-between items-center"
+					class="
+						lg:flex-grow
+						md:w-1/2
+						lg:pl-24
+						md:pl-16
+						flex flex-col
+						md:items-start
+						md:text-left
+						items-center
+						text-center
+					"
 				>
-					<p class="ml-5 text-gray-400">Mots-Clés ou catégories</p>
-					<svg
-						width="22"
-						height="22"
-						viewBox="0 0 22 22"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
+					<h1
+						class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white"
 					>
-						<path
-							d="M21 21.2882L15.9706 16.0074M18.5608 9.78039C18.5608 14.6297 14.6297 18.5608 9.78038 18.5608C4.93111 18.5608 1 14.6297 1 9.78039C1 4.93111 4.93111 1 9.78038 1C14.6297 1 18.5608 4.93111 18.5608 9.78039Z"
-							stroke="#020B2A"
-							stroke-width="2"
-						/>
-					</svg>
+						Vous cherchez un cours ?
+					</h1>
+					<p class="mb-8 leading-relaxed">
+						Vous souhaitez apprendre un métier ou une compétence et vous ne
+						savez pas si le cours est disponible ? Faites une recherche !
+					</p>
+					<div class="flex w-3/4 md:justify-start justify-center items-end">
+						<div class="relative mr-4 lg:w-full xl:w-1/2 w-2/4">
+							<input
+								type="text"
+								id="hero-field"
+								name="hero-field"
+								placeholder="Mots-clés ou catégories"
+								class="
+									w-full
+									bg-white
+									rounded-lg
+									border-none
+									text-base
+									outline-none
+									text-black
+									py-1
+									px-3
+									leading-8
+									transition-colors
+									duration-200
+									ease-in-out
+								"
+							/>
+						</div>
+						<button
+							class="
+								inline-flex
+								text-white
+								bg-white
+								border-0
+								h-10
+								w-10
+								focus:outline-none
+								hover:bg-gray-200
+								rounded-full
+								text-lg
+								flex
+								justify-center
+								items-center
+							"
+						>
+							<svg
+								width="22"
+								height="22"
+								viewBox="0 0 22 22"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M21 21.2882L15.9706 16.0074M18.5608 9.78039C18.5608 14.6297 14.6297 18.5608 9.78038 18.5608C4.93111 18.5608 1 14.6297 1 9.78039C1 4.93111 4.93111 1 9.78038 1C14.6297 1 18.5608 4.93111 18.5608 9.78039Z"
+									stroke="#020B2A"
+									stroke-width="2"
+								/>
+							</svg>
+						</button>
+					</div>
 				</div>
 			</div>
-		</div>
+		</section>
+		<section class="gradient-bg text-gray-600 body-font">
+			<div
+				class="container mx-auto flex px-5 lg:flex-row flex-col items-center"
+			>
+				<div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10">
+					<img
+						class="object-cover object-center rounded"
+						alt="hero"
+						src="~/static/woman.png"
+					/>
+				</div>
+				<div
+					class="
+						bubble
+						lg:flex-grow
+						md:w-1/2
+						lg:pl-10
+						md:pl-10
+						py-5
+						mb-4
+						flex flex-col
+						md:items-start
+						md:text-left
+						items-center
+						text-center
+						bg-white
+						rounded-lg
+					"
+				>
+					<h1
+						class="title-font sm:text-4xl text-3xl mb-4 font-medium text-black"
+					>
+						Vous souhaitez poster vos cours ?
+					</h1>
+					<p class="mb-8 leading-relaxed w-5/6">
+						Vous avez envie de transmettre votre savoir ? Postulez pour poster
+						vos cours sur notre plateforme.
+					</p>
+					<div class="flex justify-center">
+						<button
+							class="
+								inline-flex
+								text-white
+								bg-yellow-500
+								border-0
+								py-2
+								px-6
+								focus:outline-none
+								hover:bg-yellow-600
+								rounded
+								text-lg
+							"
+						>
+							Je veux devenir enseignant !
+						</button>
+					</div>
+				</div>
+			</div>
+		</section>
 	</div>
 </template>
 
@@ -152,34 +338,18 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .img-bg {
+	background-image: linear-gradient(rgba(0, 0, 0, 0.627), rgba(0, 0, 0, 0.6)),
+		url('https://images.unsplash.com/photo-1616400619175-5beda3a17896?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80');
 	background-repeat: no-repeat;
 	background-size: cover;
-	background-attachment: fixed;
-	background-position-y: 80%;
-	background-image: linear-gradient(rgba(0, 0, 0, 0.527), rgba(0, 0, 0, 0.5)),
-		url('https://images.unsplash.com/photo-1612194558554-ab692b006563?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80');
+	background-position-y: 50%;
 }
-.logo_part svg {
-	height: 100px;
-	width: 100px;
+.gradient-bg {
+	background: linear-gradient(261.28deg, #ffffff 0%, #d9d9d9 73.02%);
 }
-.search_btn svg {
-	height: 17px;
-	width: 17px;
-}
-/* Hide scrollbar for Chrome, Safari and Opera */
-.example::-webkit-scrollbar {
-	display: none;
-}
-
-/* Hide scrollbar for IE, Edge and Firefox */
-.example {
-	-ms-overflow-style: none; /* IE and Edge */
-	scrollbar-width: none; /* Firefox */
-}
-.input_search svg {
-	margin-right: 2%;
+.bubble {
+	box-shadow: 22px 23px 23px -5px rgba(0, 0, 0, 0.25);
 }
 </style>
